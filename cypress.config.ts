f1fc934ecd9fd1ecf2f3b66ae290b9961b801144
yuri <yuri.fedorov@avalon.au>
2025-02-20 22:02:21 +0300

    updates

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,10 @@
-import * as process from "process";
+import * as fs from 'fs';
+import * as path from 'path';
 require('dotenv').config();
 import { defineConfig } from 'cypress';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-import {
-  addCucumberPreprocessorPlugin,
-  beforeRunHandler,
-  afterRunHandler,
-} from '@badeball/cypress-cucumber-preprocessor';
+import {addCucumberPreprocessorPlugin, beforeRunHandler, afterRunHandler } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
-import * as fs from 'fs';
-import * as path from 'path';
 import { specPaths } from "@tests/specs";
 import { createId } from "@/tests/shared/lib/id";
 import tcms from "@yurifyodorov/tcms-data-sync";
@@ -24,20 +19,9 @@ async function setupNodeEvents(
     omitAfterScreenshotHandler: true,
   });
 
-  on("before:run", async () => {
-    await beforeRunHandler(config);
-  });
-
-  on("after:run", async () => {
-    await afterRunHandler(config);
-  });
-
-  on(
-      "file:preprocessor",
-      createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      })
-  );
+  on("before:run", async () => { await beforeRunHandler(config) });
+  on("after:run", async () => { await afterRunHandler(config) });
+  on("file:preprocessor", createBundler({ plugins: [createEsbuildPlugin(config)] }));
 
   return config;
 }
@@ -47,27 +31,18 @@ const browser = process.env.BROWSER || "chrome";
 const platform = process.platform;
 
 const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  console.error("Не найден DATABASE_URL в переменных окружения");
-}
 
-try {
-  const reportPath = path.join(__dirname, "tests/reports/cucumber-report.json");
+const reportPath = path.join(__dirname, "tests/reports/cucumber-report.json");
 
-  if (fs.existsSync(reportPath)) {
-    const rawTestData = fs.readFileSync(reportPath, "utf8");
-    const testData = JSON.parse(rawTestData);
+if (fs.existsSync(reportPath)) {
+  const rawTestData = fs.readFileSync(reportPath, "utf8");
+  const testData = JSON.parse(rawTestData);
 
-    if (databaseUrl) {
-      tcms.saveResults(runId, browser, platform, testData, databaseUrl);
-    }
-
-    tcms.sendSlackReport();
-  } else {
-    console.error(`Файл ${reportPath} не найден`);
+  if (databaseUrl) {
+    tcms.saveResults(runId, browser, platform, testData, databaseUrl);
   }
-} catch (error) {
-  console.error("Ошибка при чтении или обработке файла:", error);
+
+  tcms.sendSlackReport();
 }
 
 export default defineConfig({
@@ -83,13 +58,12 @@ export default defineConfig({
     screenshotsFolder: "tests/reports/screenshots",
     supportFile: "tests/support/e2e.ts",
     env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
       grepFilterSpecs: true,
       omitFiltered: false,
       filterSpecs: true,
     },
   },
-  defaultCommandTimeout: 4000,
-  requestTimeout: 4000,
   retries: {
     runMode: 0,
     openMode: 0,
