7ef7faab3429e3172e56b628de66b4afea917a3a
yuri <yuri.fedorov@avalon.au>
2025-02-26 17:36:09 +0300

    update collect-steps.ts

--- tests/scripts/collect-steps.ts
+++ tests/scripts/collect-steps.ts
@@ -14,7 +14,7 @@ const collectSteps = (testData: Feature[]): Map<string, Step> => {
                     stepsMap.set(stepName, {
                         id: stepId,
                         name: stepName,
-                        media: step.media || '',
+                        media: step.media || 'https://s3.timeweb.cloud/ca1c67e1-tcms-images/content/bafb763a54d1cd106a9493c2862ad757.png',
                         keyword: step.keyword || '',
                         scenarioIds: [],
                     });
